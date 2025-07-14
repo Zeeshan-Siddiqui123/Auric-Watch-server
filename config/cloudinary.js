73a0ba4ed5797bf41b99ca36f02c6f51c5ab2e29
Zeeshan Siddiqui <155655770+Zeeshan-Siddiqui123@users.noreply.github.com>
2025-07-14 18:19:40 +0500

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'auric-watch',
-    allowed_formats: ['jpg', 'jpeg', 'png'],
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
   },
 });
 
